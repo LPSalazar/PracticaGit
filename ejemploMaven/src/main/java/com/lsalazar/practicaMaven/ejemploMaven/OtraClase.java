32788657fccb01edfbe6db6e63a4f64bd74dd274
LPSalazar <Desarrolllo2@192.168.1.102>
2018-01-09 15:52:05 -0300

    se agrego mas mensaje

--- ejemploMaven/src/main/java/com/lsalazar/practicaMaven/ejemploMaven/OtraClase.java
+++ ejemploMaven/src/main/java/com/lsalazar/practicaMaven/ejemploMaven/OtraClase.java
@@ -3,6 +3,6 @@ package com.lsalazar.practicaMaven.ejemploMaven;
 public class OtraClase {
 	 public static void main( String[] args )
 	    {
-	        System.out.println( "Hello World! segunda prueba" );
+	        System.out.println( "Hello World! segunda prueba, ahora se prueba commit" );
 	    }
 }
