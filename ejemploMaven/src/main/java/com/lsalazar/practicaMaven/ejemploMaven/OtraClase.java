b0b8a76d165c09d20da671606600314a4efc0a11
LPSalazar <Desarrolllo2@192.168.1.102>
2018-01-09 15:43:18 -0300

    se agrego otra clase y se modifico el mensaje en ambas clases

---
+++ ejemploMaven/src/main/java/com/lsalazar/practicaMaven/ejemploMaven/OtraClase.java
@@ -0,0 +1,8 @@
+package com.lsalazar.practicaMaven.ejemploMaven;
+
+public class OtraClase {
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World! segunda prueba" );
+	    }
+}
