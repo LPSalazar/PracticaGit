ba8719dbab6f4a580859f19963714eb79fb4eddc
LPSalazar <Desarrolllo2@192.168.1.102>
2018-01-10 10:54:26 -0300

    esto es un ejemplo de gradle

---
+++ gradlePractica/src/main/java/Mensaje.java
@@ -0,0 +1,9 @@
+
+public class Mensaje {
+
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World! segunda prueba, ahora se prueba commitsasasasas" );
+	    }
+	 
+}
